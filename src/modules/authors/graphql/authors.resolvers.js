e4c02c1be347d51ce07ae77165e94eaf1e3d8cdd
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-06-23 07:02:27 -0300

    added author medias

--- src/modules/authors/graphql/authors.resolvers.js
+++ src/modules/authors/graphql/authors.resolvers.js
@@ -7,15 +7,23 @@ const authorsResolvers = {
             return 'Hello world!';
           },
           allAuthors: async (parent, args, ctx, info) => {
-            const fields = ctx.requestedFields.getFields(info, {})
+            const fields = ctx.requestedFields.getFields(info, { keep: ["id"], exclude: ["medias"] })            
             const sql = `SELECT ${fields.toString()} FROM authors ORDER BY id ASC`
             const authors = await ctx.orm.sequelize.query(sql, { type: QueryTypes.SELECT });
             return authors;
           }
+          
     },
     /*Mutation: {
 
     },*/
+    Author:{
+      medias: async (parent, args, ctx, info) => {
+        const sql = `SELECT * FROM authors_medias WHERE id = ${parent.id} ORDER BY id ASC`
+        const medias = await ctx.orm.sequelize.query(sql, { type: QueryTypes.SELECT });
+        return medias;
+      }
+    }
   };
   
   export default authorsResolvers;
