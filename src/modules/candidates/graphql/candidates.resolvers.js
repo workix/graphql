ebcb2ff38684954325045ce7f9b3b62c397e8c55
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-06-24 08:22:55 -0300

    fixed relation user x candidates

--- src/modules/candidates/graphql/candidates.resolvers.js
+++ src/modules/candidates/graphql/candidates.resolvers.js
@@ -4,7 +4,7 @@ import { Candidate } from '../../../models';
 const candidatesResolvers = {
     Query: {       
           allCandidates: async (parent, args, ctx, info) => {
-            const fields = ctx.requestedFields.getFields(info, { keep: ["id"], exclude: ["user", ""] })            
+            const fields = ctx.requestedFields.getFields(info, { keep: ["user_id"], exclude: ["user", ""] })            
             const sql = `SELECT ${fields.toString()} FROM candidates ORDER BY id ASC`
             const candidates = await ctx.orm.sequelize.query(sql, { type: QueryTypes.SELECT });
             return candidates;
@@ -13,7 +13,7 @@ const candidatesResolvers = {
     },
     Candidate: {
       user: async (parent, args, ctx, info) => {        
-          const users = await ctx.dataloaders.usersLoader.load({key: parent.id, info})
+          const users = await ctx.dataloaders.usersLoader.load({key: parent.user_id, info})
           return users[0];        
       }
     }
