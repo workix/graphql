0b04646f42ffc946ba505f58b6724d2242bfe304
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-06-30 09:31:24 -0300

    created repository for candidates

---
+++ src/modules/candidates/FEATURES.md
@@ -0,0 +1,20 @@
+# Users Module 
+
+## Restfull Context [Candidate]
+
+#### [Create]
+ - [] - Creating a Candidate
+
+#### [Delete]
+ - [] - Deleting Candidate by id
+
+#### [Get by Id]
+ - [] - Getting Candidate by id
+
+#### [Get All]
+- [] - GetAll with no filters
+- [] - GetAll with start, max
+- [] - Get Candidates Paginated
+
+#### [Update]
+ - [] - Updating Candidate
---
+++ src/modules/candidates/repository/candidates.repo.js
@@ -0,0 +1,79 @@
+const { QueryTypes } = require('sequelize');
+import { RequestedFields } from '../../../RequestedFields';
+import { Candidate } from '../../../models';
+import Paginator from '../../../utils/Paginator';
+import PaginatedList from '../../../utils/PaginatedList';
+
+const candidatesRepository = db => {
+    const requestedFields = new RequestedFields();
+    const getFields = info => requestedFields.getFields(info, { keep: ["id"], exclude: [] })
+    const getFieldsWithSubfields = info => requestedFields.getFieldsWithSubfields(info, { keep: ["id"], exclude: [] })
+
+    const findAll = async (info, args) => {
+        const fields = getFields(info)
+        const options = { attributes: fields, order: ['id'] }
+        if (args.start != null && args.max != null) {
+            options.offset = args.start;
+            options.limit = args.max;
+        }
+        const candidates = await Candidate.findAll(options)
+        return candidates;
+    }
+
+    const findById = async (info, args) => {
+        const fields = getFields(info)
+        const candidate = await Candidate.findOne({ where: { id: args.id }, attributes: fields })
+        return candidate;
+    }
+
+    const create = async args => {
+        const candidate = await Candidate.create(args.input)
+        await candidate.reload()
+        return candidate;
+    }
+
+    const destroy = async args => {
+        const deleted = await Candidate.destroy({ where: { id: args.id } })
+        return deleted > 0
+    }
+
+    const update = async args => {
+        const [candidates, meta] = await Candidate.update(args.input, { where: { id: args.id }, returning: true })
+
+        if (meta == 0) {
+            throw new Error(`Candidate with id: ${args.id} not found`)
+        }
+
+        const candidate = await Candidate.findOne({ where: { id: args.id } })
+
+        return candidate;
+    }
+
+    const findAllPaginated = async (info, args) => {           
+        
+        const fields = getFieldsWithSubfields(info).get("rows")              
+        
+        const totalRows = await Candidate.count()
+
+        const paginator = new Paginator(args.limit, args.page, totalRows);
+
+        const totalPages = paginator.getTotalPages();
+
+		const start = paginator.getStart();
+
+		const end = paginator.getEnd();		
+
+        const options = { attributes: fields, order: ['id'] }
+        options.offset = start - 1;
+        options.limit = args.limit;
+
+        const candidates = await Candidate.findAll(options)
+
+        const paginatedList = new PaginatedList(candidates, start, end, totalPages, paginator.getCurrentPage(), paginator.getLimitRows(), paginator.getMaxRows())
+        return paginatedList;
+    }
+
+    return { findAll, findById, create, destroy, update, findAllPaginated }
+}
+
+export default candidatesRepository;
