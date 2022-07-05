14d7cd7551ae69ce34ca6d97456b2530d5864dd7
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-07-05 13:14:48 -0300

    fixed fields for subscriber

--- src/models/subscriber.js
+++ src/models/subscriber.js
@@ -1,3 +1,4 @@
+const Sequelize = require('sequelize');
 /* jshint indent: 2 */
 
 module.exports = function(sequelize, DataTypes) {
@@ -5,25 +6,37 @@ module.exports = function(sequelize, DataTypes) {
     id: {
       type: DataTypes.BIGINT,
       allowNull: false,
-      primaryKey: true
+      primaryKey: true,
+      autoIncrement: true 
     },
     createdAt: {
       type: DataTypes.DATE,
-      allowNull: false
+      allowNull: false,
+      defaultValue: Sequelize.fn('now'),
     },
     updatedAt: {
       type: DataTypes.DATE,
-      allowNull: true
+      allowNull: true,
+      defaultValue: Sequelize.fn('now'),
     },
     uuid: {
-      type: DataTypes.STRING(255),
-      allowNull: false
+      type: DataTypes.UUID,
+      allowNull: false,
+      defaultValue: Sequelize.UUIDV4,
     },
     email: {
       type: DataTypes.STRING(255),
       allowNull: false
     }
   }, {
-    tableName: 'subscribers'
+    tableName: 'subscribers',
+    hooks: {
+      afterCreate(instance, options){
+        console.log("HOOK After create")
+      },
+      beforeUpdate(instance, options){
+        instance.updatedAt = Date.now()
+      }
+    }
   });
 };
