441190e27f5bc85e8f1f417cc94f6f540460f5e1
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-07-06 11:55:50 -0300

    realizing login and generating jwt token

--- src/modules/auth/graphql/auth.resolvers.js
+++ src/modules/auth/graphql/auth.resolvers.js
@@ -1,13 +1,30 @@
-
+const jwt = require('jsonwebtoken');
+import { User } from '../../../models';
 import authRepository from "../repository/auth.repo";
 
+
 const authResolvers = {
-  Query: {
-    
-  },
-  Mutation: {
-   
-  }  
+    Query: {
+
+    },
+    Mutation: {
+        doLogin: async (parent, args, ctx, info) => {
+            
+            const user = await User.findOne({ where: { firebaseUUID: args.input.firebaseUUID, email: args.input.email } })
+            
+            if (!user){
+                throw new Error("Email or FirebaseUUID is invalid")
+            }
+
+            const expiresIn = 900 // expires in 15min
+            const token = jwt.sign({ id: user.firebaseUUID, sub: user.email }, process.env.JWT_SECRET, {
+                expiresIn: expiresIn
+            });
+
+            return token;
+            
+        }
+    }
 };
 
 export default authResolvers;
