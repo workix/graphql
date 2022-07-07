a4c042714774f3ab5aa1d04224a00a69489fe06c
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-07-07 12:38:59 -0300

    fixed update and create elastick document

--- src/modules/users/graphql/users.resolvers.js
+++ src/modules/users/graphql/users.resolvers.js
@@ -2,49 +2,52 @@ import usersRepository from '../repository/users.repo';
 import client from '../../../factory/elastic_search_factory';
 
 const usersResolvers = {
-    Query: {       
-          allUsers: async (parent, args, ctx, info) => {            
-            const users = await usersRepository(ctx.orm).findAll(info,args)
-            return users;
-          },
-          getUserById: async (parent, args, ctx, info) => {
-            const user = await usersRepository(ctx.orm).findById(info, args)
-            return user;
-          },
-          allUsersPaginated: async (parent, args, ctx, info) => {
-            const paginatedList = await usersRepository(ctx.orm).findAllPaginated(info, args)
-            return paginatedList;
-          }                     
+  Query: {
+    allUsers: async (parent, args, ctx, info) => {
+      const users = await usersRepository(ctx.orm).findAll(info, args)
+      return users;
     },
-    Mutation: {
-        createUser: async (parent, args, ctx, info) => {
-          const user = await usersRepository(ctx.orm).create(args)
-          
-          const result = await client.index({
-            index: 'users',
-            id: user.uuid,
-            body: user
-          })
-          
-          return user;
-        },
-        deleteUser: async (parent, args, ctx, info) => {
-          const deleted = await usersRepository(ctx.orm).destroy(args)          
-          return deleted;
-        },
-        updateUser: async (parent, args, ctx, info) => {
-          const user = await usersRepository(ctx.orm).update(args)
+    getUserById: async (parent, args, ctx, info) => {
+      const user = await usersRepository(ctx.orm).findById(info, args)
+      return user;
+    },
+    allUsersPaginated: async (parent, args, ctx, info) => {
+      const paginatedList = await usersRepository(ctx.orm).findAllPaginated(info, args)
+      return paginatedList;
+    },
+    deepSearch: async (parent, args, ctx, info) => {
 
-          const result = await client.update({
-            index: "users",
-            type: "_doc",     
-            id: user.uuid,                               
-            body: {doc:{user}}
-        })
-
-          return user;
-        }
     }
+  },
+  Mutation: {
+    createUser: async (parent, args, ctx, info) => {
+      const user = await usersRepository(ctx.orm).create(args)
+
+      const result = await client.index({
+        index: 'users',
+        id: user.uuid,
+        body: user
+      })
+
+      return user;
+    },
+    deleteUser: async (parent, args, ctx, info) => {
+      const deleted = await usersRepository(ctx.orm).destroy(args)
+      return deleted;
+    },
+    updateUser: async (parent, args, ctx, info) => {
+      const user = await usersRepository(ctx.orm).update(args)
+
+      const result = await client.update({
+        index: "users",
+        type: "_doc",
+        id: user.uuid,
+        body: { doc: user }
+      })
+
+      return user;
+    }
+  }
 }
 
 export default usersResolvers;
