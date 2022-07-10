52498c75aea70fce86a1b22e6062723d48c22e42
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-07-10 12:13:33 -0300

    create comments table migration

---
+++ src/migrations/20220710145722-create_table_comments.js
@@ -0,0 +1,53 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction(transaction => {
+      return Promise.all([
+        queryInterface.createTable('comments', {
+          id: {
+            type: Sequelize.DataTypes.BIGINT,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+          },
+          createdAt: {
+            type: Sequelize.DataTypes.DATE,
+            allowNull: false,
+            defaultValue: Sequelize.fn('now'),
+          },
+          updatedAt: {
+            type: Sequelize.DataTypes.DATE,
+            allowNull: true,
+            defaultValue: Sequelize.fn('now'),
+          },
+          uuid: {
+            type: Sequelize.DataTypes.UUID,
+            allowNull: false,
+            defaultValue: Sequelize.UUIDV4,
+          },
+          email: {
+            type: Sequelize.DataTypes.STRING(255),
+            allowNull: false
+          },
+          name: {
+            type: Sequelize.DataTypes.STRING(255),
+            allowNull: false
+          },
+          text: {
+            type: Sequelize.DataTypes.TEXT,
+            allowNull: false
+          }
+        }, { transaction })
+      ]);
+    })
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction(transaction => {
+      return Promise.all([
+        queryInterface.dropTable('comments', { transaction })
+      ]);
+    })
+  }
+};
