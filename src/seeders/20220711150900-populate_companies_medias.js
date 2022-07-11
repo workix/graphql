7a8bafd0cbbe541b25461f78a62c33cf9e2cbbe8
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-07-11 12:10:15 -0300

    created seed for populate companies medias

---
+++ src/seeders/20220711150900-populate_companies_medias.js
@@ -0,0 +1,24 @@
+'use strict';
+const fs = require('fs');
+const path = require('path');
+
+const sqlCompaniesMedias = fs.readFileSync(path.resolve(__dirname, 'companies_medias.sql'), 'utf8');
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.sequelize.transaction(transaction => {
+      return Promise.all([
+        queryInterface.sequelize.query(sqlCompaniesMedias, { transaction }),        
+      ])
+    })
+  },
+
+  async down(queryInterface, Sequelize) {
+    return queryInterface.sequelize.transaction(transaction => {
+      return Promise.all([        
+        queryInterface.bulkDelete('companies_medias', {}, { transaction })
+      ])
+    })
+
+  }
+};
