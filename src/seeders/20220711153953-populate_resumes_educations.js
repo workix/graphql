23e617267751d28298c57c8717fd3fbe4a1e5608
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-07-11 12:40:51 -0300

    created seed for resume educations

---
+++ src/seeders/20220711153953-populate_resumes_educations.js
@@ -0,0 +1,24 @@
+'use strict';
+const fs = require('fs');
+const path = require('path');
+
+const sqlResumesEducations = fs.readFileSync(path.resolve(__dirname, 'resumes_educations.sql'), 'utf8');
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.sequelize.transaction(transaction => {
+      return Promise.all([
+        queryInterface.sequelize.query(sqlResumesEducations, { transaction }),        
+      ])
+    })
+  },
+
+  async down(queryInterface, Sequelize) {
+    return queryInterface.sequelize.transaction(transaction => {
+      return Promise.all([        
+        queryInterface.bulkDelete('resumes_educations', {}, { transaction })
+      ])
+    })
+
+  }
+};
