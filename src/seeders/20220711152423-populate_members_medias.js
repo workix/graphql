c995d129a91fd57cb6a9872c9b67ac4d65c8c625
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-07-11 12:25:25 -0300

    created seed for members medias

---
+++ src/seeders/20220711152423-populate_members_medias.js
@@ -0,0 +1,24 @@
+'use strict';
+const fs = require('fs');
+const path = require('path');
+
+const sqlMembersMedias = fs.readFileSync(path.resolve(__dirname, 'members_medias.sql'), 'utf8');
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.sequelize.transaction(transaction => {
+      return Promise.all([
+        queryInterface.sequelize.query(sqlMembersMedias, { transaction }),        
+      ])
+    })
+  },
+
+  async down(queryInterface, Sequelize) {
+    return queryInterface.sequelize.transaction(transaction => {
+      return Promise.all([        
+        queryInterface.bulkDelete('members_medias', {}, { transaction })
+      ])
+    })
+
+  }
+};
