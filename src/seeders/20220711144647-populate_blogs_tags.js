d523c08d58efd9500e09e109f2fe73da42538f56
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-07-11 11:48:39 -0300

    created seed populate blogs_tags

---
+++ src/seeders/20220711144647-populate_blogs_tags.js
@@ -0,0 +1,24 @@
+'use strict';
+const fs = require('fs');
+const path = require('path');
+
+const sqlBlogsTags = fs.readFileSync(path.resolve(__dirname, 'blogs_tags.sql'), 'utf8');
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.sequelize.transaction(transaction => {
+      return Promise.all([
+        queryInterface.sequelize.query(sqlBlogsTags, { transaction }),        
+      ])
+    })
+  },
+
+  async down(queryInterface, Sequelize) {
+    return queryInterface.sequelize.transaction(transaction => {
+      return Promise.all([        
+        queryInterface.bulkDelete('blogs', {}, { transaction })
+      ])
+    })
+
+  }
+};
