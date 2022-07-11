184b84c3325829cfae04a41388ff5533fae482c7
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-07-11 10:50:27 -0300

    dumped sql inserts

---
+++ src/seeders/JAAS_Role.sql
@@ -0,0 +1,2 @@
+INSERT INTO workix.JAAS_Role (name) VALUES ('Admin');
+INSERT INTO workix.JAAS_Role (name) VALUES ('Guest');
---
+++ src/seeders/members_medias.sql
@@ -0,0 +1,3 @@
+INSERT INTO workix.members_medias (id, media, url) VALUES (1, 'GitHub', 'https://github.com/frmichetti');
+INSERT INTO workix.members_medias (id, media, url) VALUES (1, 'Twitter', 'https://twitter.com/frmichetti');
+INSERT INTO workix.members_medias (id, media, url) VALUES (1, 'Linkedin', 'https://br.linkedin.com/pub/felipe-rodrigues-michetti/105/61/511');
