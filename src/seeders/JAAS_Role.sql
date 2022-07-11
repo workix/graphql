f7e3bce52a5d81a65acf1c1fa61135909d57421a
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-07-11 12:15:14 -0300

    creed seed for jaas_role

--- src/seeders/JAAS_Role.sql
+++ src/seeders/JAAS_Role.sql
@@ -1,2 +1,2 @@
-INSERT INTO workix.JAAS_Role (name) VALUES ('Admin');
-INSERT INTO workix.JAAS_Role (name) VALUES ('Guest');
+INSERT INTO JAAS_Role (name) VALUES ('Admin');
+INSERT INTO JAAS_Role (name) VALUES ('Guest');
