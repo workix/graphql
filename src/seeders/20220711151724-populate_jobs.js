4f8dd4d7564f71f2227f8c52f04e379d47ebcb42
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-07-11 12:19:16 -0300

    created seed for jobs

---
+++ src/seeders/20220711151724-populate_jobs.js
@@ -0,0 +1,24 @@
+'use strict';
+const fs = require('fs');
+const path = require('path');
+
+const sqlJobs = fs.readFileSync(path.resolve(__dirname, 'jobs.sql'), 'utf8');
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.sequelize.transaction(transaction => {
+      return Promise.all([
+        queryInterface.sequelize.query(sqlJobs, { transaction }),        
+      ])
+    })
+  },
+
+  async down(queryInterface, Sequelize) {
+    return queryInterface.sequelize.transaction(transaction => {
+      return Promise.all([        
+        queryInterface.bulkDelete('jobs', {}, { transaction })
+      ])
+    })
+
+  }
+};
