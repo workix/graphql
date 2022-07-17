063e195e79ea286b77dc73b38b6f81a8c476d7d6
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-07-17 15:45:46 -0300

    added spec for all users paginated

--- tests/graphql/queries/users.spec.js
+++ tests/graphql/queries/users.spec.js
@@ -74,7 +74,7 @@ describe("GRAPHQL - [Users]", () => {
         }
         `
       const { user } = await request(endpoint, query, { id: 1 })
-      
+
       expect(user).toHaveProperty('id', "1");
       expect(user).toHaveProperty('createdAt');
       expect(user).toHaveProperty('updatedAt');
@@ -83,6 +83,42 @@ describe("GRAPHQL - [Users]", () => {
       expect(user).toHaveProperty('email');
       expect(user).toHaveProperty('firebaseMessageToken');
       expect(user).toHaveProperty('firebaseUUID');
+    })
+
+    it("AllUsers paginated", async () => {
+      const query = gql`query AllUsersPaginated($page: Int!, $limit: Int!) {
+            paginated: allUsersPaginated(page: $page, limit: $limit) {
+              users {
+                id
+                createdAt
+                updatedAt
+                uuid
+                activated
+                email
+                firebaseMessageToken
+                firebaseUUID
+              }
+              start
+              end
+              totalPages
+              currentPage
+              limitRows
+              maxRows
+            }
+          }          
+          `
+      const { paginated } = await request(endpoint, query, { page: 1, limit: 10 })
+
+      expect(paginated.users.length).toBeGreaterThan(0);
+      expect(paginated.users.length).toBeLessThan(11);
+      expect(paginated.users[0]).toHaveProperty('id');
+      expect(paginated.users[0]).toHaveProperty('createdAt');
+      expect(paginated.users[0]).toHaveProperty('updatedAt');
+      expect(paginated.users[0]).toHaveProperty('uuid');
+      expect(paginated.users[0]).toHaveProperty('activated');
+      expect(paginated.users[0]).toHaveProperty('email');
+      expect(paginated.users[0]).toHaveProperty('firebaseMessageToken');
+      expect(paginated.users[0]).toHaveProperty('firebaseUUID');
     })
 
 
