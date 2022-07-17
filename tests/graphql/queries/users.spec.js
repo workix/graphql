2a4dbf3383555b30fe428926d1f7e94508248799
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-07-17 15:38:38 -0300

    added spec for get user by id

--- tests/graphql/queries/users.spec.js
+++ tests/graphql/queries/users.spec.js
@@ -3,9 +3,9 @@ import { request, gql } from 'graphql-request'
 const endpoint = `http://localhost:4000/graphql`
 
 describe("GRAPHQL - [Users]", () => {
-    describe("Queries", () => {
-        it("AllUsers with no Parameters", async () => {
-            const query = gql`{
+  describe("Queries", () => {
+    it("AllUsers with no Parameters", async () => {
+      const query = gql`{
                 users: allUsers {
                   id
                   createdAt
@@ -18,21 +18,21 @@ describe("GRAPHQL - [Users]", () => {
                 }
               }
               `
-            const { users } = await request(endpoint, query)
+      const { users } = await request(endpoint, query)
 
-            expect(users.length).toBeGreaterThanOrEqual(100);
-            expect(users[0]).toHaveProperty('id');
-            expect(users[0]).toHaveProperty('createdAt');
-            expect(users[0]).toHaveProperty('updatedAt');
-            expect(users[0]).toHaveProperty('uuid');
-            expect(users[0]).toHaveProperty('activated');
-            expect(users[0]).toHaveProperty('email');
-            expect(users[0]).toHaveProperty('firebaseMessageToken');
-            expect(users[0]).toHaveProperty('firebaseUUID');
-        })
+      expect(users.length).toBeGreaterThanOrEqual(100);
+      expect(users[0]).toHaveProperty('id');
+      expect(users[0]).toHaveProperty('createdAt');
+      expect(users[0]).toHaveProperty('updatedAt');
+      expect(users[0]).toHaveProperty('uuid');
+      expect(users[0]).toHaveProperty('activated');
+      expect(users[0]).toHaveProperty('email');
+      expect(users[0]).toHaveProperty('firebaseMessageToken');
+      expect(users[0]).toHaveProperty('firebaseUUID');
+    })
 
-        it("AllUsers with Parameters", async () => {
-          const query = gql`query AllUsersWithParams($start: Int, $max: Int) {
+    it("AllUsers with Parameters", async () => {
+      const query = gql`query AllUsersWithParams($start: Int, $max: Int) {
             users: allUsers(start: $start, max: $max) {
               id
               createdAt
@@ -45,21 +45,51 @@ describe("GRAPHQL - [Users]", () => {
             }
           }
           `
-          const { users } = await request(endpoint, query, {start: 0, max: 1})
+      const { users } = await request(endpoint, query, { start: 0, max: 1 })
 
-          expect(users.length).toBeGreaterThan(0);
-          expect(users.length).toBeLessThan(2);
-          expect(users[0]).toHaveProperty('id');
-          expect(users[0]).toHaveProperty('createdAt');
-          expect(users[0]).toHaveProperty('updatedAt');
-          expect(users[0]).toHaveProperty('uuid');
-          expect(users[0]).toHaveProperty('activated');
-          expect(users[0]).toHaveProperty('email');
-          expect(users[0]).toHaveProperty('firebaseMessageToken');
-          expect(users[0]).toHaveProperty('firebaseUUID');
-      })
+      expect(users.length).toBeGreaterThan(0);
+      expect(users.length).toBeLessThan(2);
+      expect(users[0]).toHaveProperty('id');
+      expect(users[0]).toHaveProperty('createdAt');
+      expect(users[0]).toHaveProperty('updatedAt');
+      expect(users[0]).toHaveProperty('uuid');
+      expect(users[0]).toHaveProperty('activated');
+      expect(users[0]).toHaveProperty('email');
+      expect(users[0]).toHaveProperty('firebaseMessageToken');
+      expect(users[0]).toHaveProperty('firebaseUUID');
     })
 
-    
+    it("getUserById", async () => {
+      const query = gql`query getUserById($id: ID!) {
+          user: getUserById(id: $id) {
+            id
+            createdAt
+            updatedAt
+            uuid
+            activated
+            email
+            firebaseMessageToken
+            firebaseUUID
+          }
+        }
+        `
+      const { user } = await request(endpoint, query, { id: 1 })
+      
+      expect(user).toHaveProperty('id', "1");
+      expect(user).toHaveProperty('createdAt');
+      expect(user).toHaveProperty('updatedAt');
+      expect(user).toHaveProperty('uuid');
+      expect(user).toHaveProperty('activated');
+      expect(user).toHaveProperty('email');
+      expect(user).toHaveProperty('firebaseMessageToken');
+      expect(user).toHaveProperty('firebaseUUID');
+    })
+
+
+
+
+  })
+
+
 })
 
