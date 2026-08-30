5d505951213fa1ed65f45612cb5322963b26d145
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2026-08-29 23:50:26 -0300

    feat(client): adiciona scaffolding inicial do Vue 3 + Vite no frontend/client

---
+++ frontend/client/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="workix-client-app">
+    <router-view />
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style>
+/* Estilos globais do aplicativo Cliente */
+</style>
