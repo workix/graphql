6a30e7428996efa5eb7ffa90355455e6174f00d5
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2026-08-31 11:38:29 -0300

    feat(auth): refatora authResolvers para integracao com authRepository e validacao de firebase uuid

--- src/modules/auth/graphql/auth.resolvers.ts
+++ src/modules/auth/graphql/auth.resolvers.ts
@@ -1,42 +1,33 @@
 import { authResolver } from './../../../composable_resolvers/auth-resolver';
 import { compose } from './../../../composable_resolvers/composable.resolver';
 import { verifyTokenResolver } from './../../../composable_resolvers/verify-token-resolver';
-
-const authGuard = [authResolver, verifyTokenResolver]
-
-
-const jwt = require('jsonwebtoken');
-import { User, Company, Candidate, Resume } from '../../../models';
 import authRepository from "../repository/auth.repo";
 
+const authGuard = [authResolver, verifyTokenResolver];
 
 const authResolvers = {
     Query: {
         aboutMe: compose(...authGuard)(async (parent, args, ctx, info) => {
-            const user = await User.findOne({ where: { firebase_uuid: ctx.user.firebase_uuid, email: ctx.user.email }, raw: true })
-            const company = await Company.findOne({ where: { user_id: user.id } })
-            const candidate = await Candidate.findOne({ where: { user_id: user.id } })
-            const resume = candidate ? await Resume.findOne({where: {candidate_id: candidate.id}}) : null
+            const repo = authRepository(ctx?.orm);
+            const userContext = ctx?.user;
 
-            return { user, company, candidate, resume }
+            if (!userContext?.firebase_uuid || !userContext?.email) {
+                throw new Error("Unauthorized! Token payload is invalid");
+            }
+
+            return await repo.getAboutMe(userContext.firebase_uuid, userContext.email);
         })
     },
     Mutation: {
         doLogin: async (parent, args, ctx, info) => {
-
-            const user = await User.findOne({ where: { firebase_uuid: args.input.firebaseUUID, email: args.input.email } })
+            const repo = authRepository(ctx?.orm);
+            const user = await repo.findByFirebaseUUIDAndEmail(args.input.firebaseUUID, args.input.email);
 
             if (!user) {
-                throw new Error("Email or FirebaseUUID is invalid")
+                throw new Error("Email or FirebaseUUID is invalid");
             }
 
-            const expiresIn = 900 // expires in 15min
-            const token = jwt.sign({ id: user.firebase_uuid, sub: user.email }, process.env.JWT_SECRET, {
-                expiresIn: expiresIn
-            });
-
-            return token;
-
+            return repo.generateToken(user, 900);
         }
     }
 };
