10dc356aea381b43cf86748823de72d14ca5b8ae
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2026-09-06 12:26:48 -0300

    test(android-e2e): implementar testes de instrumentacao de login e validacao no app android

--- android/app/src/androidTest/java/br/com/codecode/workix/android/ui/LoginActivityTest.kt
+++ android/app/src/androidTest/java/br/com/codecode/workix/android/ui/LoginActivityTest.kt
@@ -1,8 +1,13 @@
 package br.com.codecode.workix.android.ui
 
 import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import br.com.codecode.workix.android.R
 import br.com.codecode.workix.android.ui.auth.LoginActivity
 import org.junit.Assert.assertNotNull
 import org.junit.Test
@@ -18,6 +23,22 @@ class LoginActivityTest {
         scenario.onActivity { activity ->
             assertNotNull("LoginActivity deve inicializar com sucesso", activity)
         }
+        scenario.close()
+    }
+
+    @Test
+    fun testLoginValidationAndInteraction() {
+        val scenario = ActivityScenario.launch(LoginActivity::class.java)
+        
+        onView(withId(R.id.email)).check(matches(isDisplayed()))
+        onView(withId(R.id.password)).check(matches(isDisplayed()))
+        onView(withId(R.id.btn_login)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.email)).perform(typeText("candidato@workix.com"), closeSoftKeyboard())
+        onView(withId(R.id.password)).perform(typeText("SenhaForte123!"), closeSoftKeyboard())
+
+        onView(withId(R.id.btn_login)).perform(click())
+
         scenario.close()
     }
 }
